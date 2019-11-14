4c82a07c700b4571f64b60f8473511fa9300ce63
Samat22 <57350400+Samat22@users.noreply.github.com>
2019-11-14 17:08:46 +0600

    Update Warrior.java

--- src/com/company/Warrior.java
+++ src/com/company/Warrior.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Warrior extends Player {
+  private String attackType;
+
+
+    public String getAttackType() {
+        return attackType;
+    }
+
+    public void setAttackType(String attackType) {
+        this.attackType = attackType;
+    }
 }
