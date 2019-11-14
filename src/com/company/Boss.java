ca59db77074b672d715a6c4e941665734d503364
Samat22 <57350400+Samat22@users.noreply.github.com>
2019-11-14 17:07:40 +0600

    Update Boss.java

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Boss extends Player {
+  private  String defenceType;
+
+
+    public String getDefenceType() {
+        return defenceType;
+    }
+
+    public void setDefenceType(String defenceType) {
+        this.defenceType = defenceType;
+    }
 }
+
